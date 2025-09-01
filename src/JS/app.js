d905fe15709dc38f8c69752e11d7ee5ac913f61e
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-09-01 12:23:00 -0300

    Feat: Atualizar/Deletar

--- src/JS/app.js
+++ src/JS/app.js
@@ -63,10 +63,11 @@ window.onload = function(){
   salvaDados()
   displayJogadoras()
 
-  document.querySelector(".card-container").addEventListener("click", favoritar)
+  document.querySelector(".card-container").addEventListener("click", ClickCard)
   document.querySelector("#adicionar").addEventListener("click", abrirModal)
   document.querySelector("#fecha-modal").addEventListener("click", fecharModal)
-  document.querySelector("#formulario").addEventListener("submit", adicionaJogadora)
+  document.querySelector("#salva-jogadora").addEventListener("click", adicionaJogadora)
+  document.querySelector("#atualiza-jogadora").addEventListener("click", atualizaInfo)
 }
 
 function displayJogadoras(){
@@ -93,7 +94,11 @@ function displayJogadoras(){
           </div>
           <div class='extra'>
             <p>${posicaoAbreviada}</p>
-            <button data-index='${index}'>${jogadora.favorita ? `<img src='src/assets/favorito.png'>` : `<img src='src/assets/neutro.png'>`}</button>
+            <button data-action='favoritar' data-index='${index}'>${jogadora.favorita ? `<img src='src/assets/favorito.png'>` : `<img src='src/assets/neutro.png'>`}</button>
+          </div>
+          <div class='btns-UD'>
+            <button class='ud-btn' data-action='atualizar' data-index='${index}'>Upd</button>
+            <button class='ud-btn' data-action='deletar' data-index='${index}'>Del</button>
           </div>
         </div>
         `
@@ -101,17 +106,19 @@ function displayJogadoras(){
     })
 }
 
-function favoritar(e){
-  const jogadoraFavoritada = e.target.closest("button")
-  index = jogadoraFavoritada.dataset.index
-  if(dadosBase[index].favorita){
-    dadosBase[index].favorita = false
-  }
-  else{
-    dadosBase[index].favorita = true
-  }
-  salvaDados()
-  displayJogadoras()
+function ClickCard(e){
+  const clicked = e.target.closest("button")
+  if(!clicked) return
+
+  index = clicked.dataset.index
+  acao = clicked.dataset.action
+
+  if(acao === "favoritar") favoritar(index)
+
+  if(acao === "atualizar") atualizaCard(index)
+
+  if(acao === "deletar") deletarCard(index)
+
 }
 
 function salvaDados() {
@@ -127,12 +134,25 @@ function carregaDados() {
 
 function abrirModal(){
   const modal = document.querySelector("#form-modal")
-  modal.showModal()
+  if(!modal.open){
+    modal.showModal()
+  }
 }
 
 function fecharModal(){
   const modal = document.querySelector("#form-modal")
-  modal.close()
+  if(modal.open){
+    modal.close()
+  }
+  resetAdicao()
+}
+
+function resetAdicao(){
+  document.querySelector("#formulario").reset()
+  document.querySelector("#modal-titulo").innerHTML = "Adicionar Jogadora"
+
+  document.querySelector("#salva-jogadora").style.display = "inline-block"
+  document.querySelector("#atualiza-jogadora").style.display = "none"
 }
 
 function adicionaJogadora(e){
@@ -145,6 +165,11 @@ function adicionaJogadora(e){
   const gols = document.querySelector("#gol-input").value
   const assintencias = document.querySelector("#ass-input").value
   const jogos = document.querySelector("#jogos-input").value
+
+  if(!nome || !clube || !posicao || isNaN(gols) || isNaN(assintencias) || isNaN(jogos)){
+    alert("Preencha todos os campos do formulário (imagem não é obrigatório)")
+    return
+  }
 
   novaJogadora = {
     "nome": nome,
@@ -163,7 +188,84 @@ function adicionaJogadora(e){
   document.querySelector("#formulario").reset()
   displayJogadoras()
 
-  document.querySelector("#form-modal").close()
+  fecharModal()
 
   alert(`Jogadora ${nome} cadastrada com sucesso!`)
+}
+
+function favoritar(){
+  if(dadosBase[index].favorita){
+    dadosBase[index].favorita = false
+  }
+  else{
+    dadosBase[index].favorita = true
+  }
+  salvaDados()
+  displayJogadoras()
+}
+
+function deletarCard(index){
+  const escolha = confirm(`Tem certeza que deseja excluir a jogadora ${dadosBase[index].nome}`)
+  if(escolha){
+    dadosBase.splice(index, 1)
+    alert("Jogadora excluída com sucesso!")
+  }
+  salvaDados()
+  displayJogadoras()
+}
+
+let indexEdicao = undefined
+
+function atualizaCard(index){
+  tituloModal = document.querySelector("#modal-titulo")
+  tituloModal.innerHTML = `Atualizar dados ${dadosBase[index].nome}`
+  abrirModal()
+  document.querySelector("#nome-input").value = dadosBase[index].nome
+  document.querySelector("#clube-input").value = dadosBase[index].clube
+  document.querySelector("#imagem-input").value = dadosBase[index].foto
+  document.querySelector("#gol-input").value = dadosBase[index].gols
+  document.querySelector("#ass-input").value = dadosBase[index].assistencias
+  document.querySelector("#jogos-input").value = dadosBase[index].jogos
+  document.querySelector("#posicao-select").value = dadosBase[index].posicao
+
+  document.querySelector("#salva-jogadora").style.display = "none"
+  document.querySelector("#atualiza-jogadora").style.display = "inline-block"
+
+  indexEdicao = index
+}
+
+function atualizaInfo(e){
+  e.preventDefault()
+
+  const nome = document.querySelector("#nome-input").value
+  const clube = document.querySelector("#clube-input").value
+  const imagem = document.querySelector("#imagem-input").value
+  const posicao = document.querySelector("#posicao-select").value
+  const gols = document.querySelector("#gol-input").value
+  const assintencias = document.querySelector("#ass-input").value
+  const jogos = document.querySelector("#jogos-input").value
+
+  if(!nome || !clube || !posicao || isNaN(gols) || isNaN(assintencias) || isNaN(jogos)){
+    alert("Preencha todos os campos do formulário (imagem não é obrigatório)")
+    return
+  }
+
+  const escolha = confirm(`Tem certeza que deseja atualizar os dados dessa jogadora?`)
+  if(escolha){
+    dadosBase[indexEdicao].nome = nome
+    dadosBase[indexEdicao].clube = clube
+    dadosBase[indexEdicao].foto = imagem
+    dadosBase[indexEdicao].posicao = posicao
+    dadosBase[indexEdicao].gols = gols
+    dadosBase[indexEdicao].jogos = jogos
+    dadosBase[indexEdicao].assistencias = assintencias
+
+    alert(`${nome} atualizada com sucesso`)
+    indexEdicao = undefined
+
+    salvaDados()
+    displayJogadoras()
+    resetAdicao()
+    fecharModal()
+  }
 }
